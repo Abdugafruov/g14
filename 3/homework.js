885a5c3c01b20454bb9a07dc845da1480aeea972
Bilol <bilolgafuroff@gmail.com>
2023-07-12 18:33:02 +0500

    IF Else

---
+++ 3/homework.js
@@ -0,0 +1,81 @@
+// 1 ?
+// let result;
+
+// let a = 1;
+// let b = 2;
+// a + b < 4 ? result = 'Below' : result = 'Over';
+// console.log(result);
+
+//2 ?
+// if ("0") {
+//     alert( "Hello" );
+// };
+
+
+//! 1
+// let a = -2;
+
+// a < -0 ? console.log(++a) : console.log(a);
+
+//! 2
+// let a = 5;
+
+// a < -0 ? console.log(++a) : console.log(a - 2);
+
+//! 3
+// let a = -5;
+
+// if (a == 0) console.log(10);
+// else if (a < -0) console.log(++a);
+// else console.log(a - 2);
+
+//! 4
+// let a = 4;
+// let b = -2;
+// let c = -1;
+
+// if (a < -0) console.log(a);
+// if (b < -0) console.log(b);
+// if (c < -0) console.log(c);
+
+//! 5
+// let a = 4;
+// let b = -2;
+// let c = -1;
+
+// if (a < -0) console.log(a + " Muspat son");
+// else console.log(a + " Manfiy son");
+// if (b < -0) console.log(b + " Muspat son");
+// else console.log(b + " Manfiy son");
+// if (c < -0) console.log(c + " Muspat son");
+// else console.log(c + " Manfiy son");
+
+//! 6
+// let a = 8;
+// let b = 6;
+
+// a > b ? console.log(a) : console.log(b);
+
+//! 7
+// let a = 8;
+// let b = 6;
+
+// a < b ? console.log(a) : console.log(b);
+
+//! 8
+// let a = 8;
+// let b = 7;
+
+// a > b ? console.log(a) : console.log(b);
+
+//! 9
+// let a = 12;
+// let b = 6;
+// let c;
+
+// if (a > b) {
+//     c = b;
+//     b = a;
+//     a = c;
+//     console.log(`A ${a}`,`B ${b}`);
+// }else console.log(`A ${a}`,`B ${b}`);
