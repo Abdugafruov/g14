7e12f35058954a0d920aeeeb8b6e8db6b516b1a2
Bilol <bilolgafuroff@gmail.com>
2023-07-13 14:38:39 +0500

    switch case

---
+++ 4/homework.js
@@ -0,0 +1,109 @@
+//! 1
+// let day = 4;
+
+// switch(day) {
+//     case 1 : console.log("Dushanba"); break;
+//     case 2 : console.log("Seshanba"); break;
+//     case 3 : console.log("Chorshanba"); break;
+//     case 4 : console.log("Payshanba"); break;
+//     case 5 : console.log("Juma"); break;
+//     case 6 : console.log("Shanba"); break;
+//     case 7 : console.log("Yakshanba"); break;
+//     default: console.log("Eroor 404");
+// }
+
+
+//! 2
+// let k = 4;
+
+// switch(k) {
+//     case 1 : console.log("1 - yomon"); break;
+//     case 2 : console.log("2 - qoniqars"); break;
+//     case 3 : console.log("3 - qoniqarli"); break;
+//     case 4 : console.log("4 - yahshi"); break;
+//     case 5 : console.log("5 - a'lo"); break;
+//     default: console.log("Eroor 404");
+// }
+
+
+//! 3
+
+// function seazon(a){
+//         switch(a){
+//             case 1:console.log("qish");
+//             break;
+//             case 2:console.log("qish");
+//             break;
+//             case 12:console.log("qish");
+//             break;
+//             case 3:console.log("bahor");
+//             break;
+//             case 4:console.log("bahor");
+//             break;
+//             case 5:console.log("bahor");
+//             break;
+//             case 6:console.log("yoz");
+//             break;
+//             case 7:console.log("yoz");
+//             break;
+//             case 8:console.log("yoz");
+//             break;
+//             case 9:console.log("kuz");
+//             break;
+//             case 10:console.log("kuz");
+//             break;
+//             case 11:console.log("kuz");
+//             break;
+            
+//         }
+//         }
+//         seazon(2);
+
+
+//! 4
+
+// function seazon(a){
+//         switch(a){
+//             case 1:console.log("31");
+//             break;
+//             case 2:console.log("28");
+//             break;
+//             case 12:console.log("31");
+//             break;
+//             case 3:console.log("31");
+//             break;
+//             case 4:console.log("30");
+//             break;
+//             case 5:console.log("31");
+//             break;
+//             case 6:console.log("30");
+//             break;
+//             case 7:console.log("31");
+//             break;
+//             case 8:console.log("31");
+//             break;
+//             case 9:console.log("30");
+//             break;
+//             case 10:console.log("31");
+//             break;
+//             case 11:console.log("30");
+//             break;
+            
+//         }
+//         }
+//         seazon(2);
+
+//!  5
+// function arithmetic(c,a,b){
+//     switch(c){
+//         case "+":console.log(a+b);
+//         break;
+//         case "-":console.log(a-b);
+//         break;
+//         case "*":console.log(a*b);
+//         break;
+//         case "/":console.log(a/b);
+//         break;
+//     }
+//     }
+//     arithmetic("-",10,5);
