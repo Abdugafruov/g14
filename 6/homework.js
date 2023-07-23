ad2290d6e1258d3576b72cf9a1b7d8214428a7ea
Bilol <bilolgafuroff@gmail.com>
2023-07-23 09:22:08 +0500

    function

---
+++ 6/homework.js
@@ -0,0 +1,23 @@
+// function getMax(a, b) {
+//     if (a > b) return a;
+//     else if (a < b) return b;
+//     else return 'Ikkalasi teng'
+// }
+// console.log(getMax(1, 2));
+
+
+
+// function binary(a){
+//     var binary = '';
+//     while (a>0) {
+//         binary = (a%2) + binary;
+//         binary = (a%2) + binary;
+//         a = Math.floor(a/2);
+//     }; return binary;
+// }
+// console.log(binary(2));
+
+// function pow(a, b){
+//     console.log(a ** b);
+// };
+// pow(2, 3)
