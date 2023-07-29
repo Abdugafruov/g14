66c5f77ba56b94db8110f4b381daa47bb17de541
Bilol <bilolgafuroff@gmail.com>
2023-07-29 15:00:49 +0500

    Numbers

---
+++ 9/index.js
@@ -0,0 +1,88 @@
+//! Number
+// let num = "23.4b5"
+
+// console.log(typeof num);
+// console.log(+num);
+// console.log(Number(num));
+// console.log(parseInt(num)); 
+// console.log( parseFloat(num));
+
+// ? _
+// let num1 = 1000000;
+// let num2 = 1_000_000;
+
+// console.log(num1 + 1);
+// console.log(num2 + 1);
+
+//? 1e6
+// let num1 = 1000000;
+// let num2 = 1e6;
+
+// console.log(num1);
+// console.log(num2);
+
+//? toFixed()
+// let num = 1.2345;
+
+// console.log(num.toFixed(3));
+
+//? toPrecision()
+// let num = 1.2345;
+
+// console.log(num.toPrecision(2));
+
+//? isNaN()
+// let num1 = NaN;
+// let num2 = "hello";
+// let num3 = 123;
+
+// console.log(isNaN(num1));
+// console.log(isNaN(num2));
+// console.log(isNaN(num3));
+
+//? ===, Object.is()
+// console.log(Object.is(23, 23));
+
+//! Math
+//? PI
+// console.log(Math.PI);
+//? abs
+// let num1 = -10;
+// let num2 = 10;
+
+// console.log(Math.abs(num1));
+// console.log(Math.abs(num2));
+//? ** , pow
+// console.log(Math.pow(2, 3));
+//? pow
+// console.log(Math.sqrt(16));
+//? round
+// let num = 1.5;
+
+// console.log(Math.round(num));
+//? trunc
+// let num = 1.99999;
+
+// console.log(Math.trunc(num));
+//? ceil
+// let num = 1.000000000000001
+
+// console.log(Math.ceil(num));
+//? floor
+// let num = 1.999999;
+
+// console.log(Math.floor(num));
+//? sign
+// let num1 = 100;
+// let num2 = -20;
+// let num3 = 0;
+// let num4 = -0;
+
+// console.log(Math.sign(num1));
+// console.log(Math.sign(num2));
+// console.log(Math.sign(num3));
+// console.log(Math.sign(num4));
+//? random
+// console.log(Math.random());
+// console.log(Math.random() * 10 + 1);
+// console.log(Math.floor(Math.random() * 26 + 25)); //25 > 50
