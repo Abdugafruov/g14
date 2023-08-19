e2dede38132075a7ec6fd97170b88a582c8e0d91
Bilol <bilolgafuroff@gmail.com>
2023-08-19 15:48:35 +0500

    Destructure | JSON | new Date()

--- 14/homework.js
+++ 14/homework.js
@@ -1,41 +1,29 @@
-let months = [
-    "Yanvar",
-    "Fevral",
-    "Mart",
-    "Aprel",
-    "May",
-    "Iyun",
-    "Iyul",
-    "Avgust",
-    "Sentabr",
-    "Oktabr",
-    "Noyabr",
-    "Dekabr",
-  ];
-  
-  let days = [
-    "Yakshanba",
-    "Dushanba",
-    "Seyshanba",
-    "Chorshanba",
-    "Payshanba",
-    "Juma",
-    "Shanba",
-  ];
-  
-  let moment = () => {
-    let date = new Date();
-    return {
-      format(param) {
-        switch (param) {
-            case "LT": return `${date.getHours()} : ${date.getMinutes()}`;
-            case "LTS": return `${date.getHours()}: ${date.getMinutes()}:${date.getSeconds()}`;
-            case "L": return `${date.getMonth}/${date.getDate()}/${date.getFullYear()}`;
-            case "LL": return `${months[date.getMonth()]}, ${date.getDate()} ${date.getFullYear()}`;
-            case "LLL": return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()} ${date.getHours() >= 13 ? "PM" : "AM"}`;
-            case "LLLL": return `${days[date.getDay()]} ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()} ${date.getHours() >= 13 ? "PM" : "AM"}`;
-        }
-      },
-    };
-  };
-  console.log(moment().format("LT"));
+// let months = [
+//     "Yanvar",
+//     "Fevral",
+//     "Mart",
+//     "Aprel",
+//     "May",
+//     "Iyun",
+//     "Iyul",
+//     "Avgust",
+//     "Sentabr",
+//     "Oktabr",
+//     "Noyabr",
+//     "Dekabr",
+//   ];
+
+// let moment = () => {
+//   let date = new Date;
+//   return {
+//     format (a) {
+//       switch(a){
+//         case "dddd": return months[date.getMonth()];
+//         case "MMMM Do YYYY, h:mm:ss a": return `${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()} ${date.getHours() >= 13 ? "PM" : "AM"}`;
+//         case "MMM Do YY": return `${months[date.getMonth()]} ${date.getDate() + "th"}`
+//         default: return date;
+//       }
+//     }
+//   }
+// }
+// console.log(moment().format());
