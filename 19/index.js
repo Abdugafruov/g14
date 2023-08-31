ae84aa49c57f1ca29c46baba792e2eb4c88a9de8
Bilol <bilolgafuroff@gmail.com>
2023-08-31 11:23:18 +0500

    setTimeout | setInterval | clearTimeout, clearInterval

--- 19/index.js
+++ 19/index.js
@@ -2,20 +2,17 @@
 // console.log(1);
 // console.log(2);
 // console.log(3);
-
 //! Asinxron kod
 //? asinxron kod
 //? ...
 //? ...
 //? ...
 //? ...
-
 //! setTimeout(() => {}, time)
 // setTimeout(() => {
 //   console.log(1);
 // }, 5000);
 // console.log("webbrain");
-
 //! savollar
 // console.log(1);
 // setTimeout(() => console.log(2), 0);
@@ -25,14 +22,12 @@
 // console.log(6);
 // console.log(7);
 //! event loop
-
 //! setInterval
 // let i = 0;
 // setInterval(() => {
 //   i++;
 //   console.log(i);
 // }, 1000);
-
 //! soat
 // let h = 0;
 // let m = 0;
@@ -49,3 +44,22 @@
 //   }
 //   console.log(`${h}:${m}:${s}`);
 // }, 1000);
+
+//! clearInterval, clearTimeout
+// let i = 0;
+
+// let interval = setInterval(() => {
+//   i++;
+//   console.log(i);
+// 	if(i >= 5) clearInterval(interval)
+// }, 1000);
+
+// setTimeout(() => {
+//   clearInterval(interval);
+// }, 30 * 1000);
+
+// let after2 = setTimeout(() => {
+//   console.log(2);
+// 	clearTimeout(after2);
+//   console.log(3);
+// }, 2000);
